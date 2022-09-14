f7d9e6f2ace7ca6800ffa57b7a5a5b8743176c00
KofiSikaEgyir <kofisika.egyir@gmail.com>
2022-09-14 13:58:54 -0500

    Authentication

--- modules/users/auth.controller.js
+++ modules/users/auth.controller.js
@@ -1,4 +1,20 @@
 const User = require("./user.model");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const generateToken = (user) => {
+    const token = jwt.sign({id: user._id, email: user.email},
+        "21a26fdba6bbfd5e4df32024dabd48dc2c1d3764ac21ed1bc8df41ee2603b381",
+        {
+            expiresIn: "1h",
+        }
+    );
+
+    return  {
+        token,
+        user,
+    }; 
+};
 
 exports.register = async (req, res) => {
     const {email, password} = req.body;
@@ -9,6 +25,32 @@ exports.register = async (req, res) => {
         return res.status(400).json({"error": "Email already exist "})
     }
 
-    const user = await User.create({...req.body});
-    res.status(201).json({user})
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const user = await User.create({ ...req.body, password:hashedPassword });
+
+    //generate token
+    const token = generateToken(user);
+
+    res.status(201).json({token});
 };
+
+exports.login = async (req, res) => {
+    const {email, password} = req.body;
+
+    let user = await User.findOne({email});
+    if (!user) {
+        return res.status(400).json({"msg": "Invalid credentials"});
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+        return res.status(400).json({"msg": "Invalid credentials"});
+    }
+
+   //generate token
+  const token =generateToken(user);
+
+res.status(200).json({token});
+};
+
